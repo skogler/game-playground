384c31418ff20c0c0d912f292ff8a46024d7ae60
Zeheron <stiboller@gmail.com>
2012-06-10 20:31:16 +0200

    stuff

---
+++ src/utils/UIDGenerator.cpp
@@ -0,0 +1,26 @@
+/*
+ * UIDGenerator.cpp
+ *
+ *  Created on: Jun 10, 2012
+ *      Author: zeheron
+ */
+
+#include "UIDGenerator.h"
+
+UIDGenerator* UIDGenerator::uidGen = 0;
+
+UIDGenerator::UIDGenerator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+UIDGenerator* UIDGenerator::instance() {
+	if (!uidGen) {
+		uidGen = new UIDGenerator();
+	}
+	return uidGen;
+}
+
+UIDGenerator::~UIDGenerator() {
+	// TODO Auto-generated destructor stub
+}
---
+++ src/utils/UIDGenerator.h
@@ -0,0 +1,26 @@
+/*
+ * UIDGenerator.h
+ *
+ *  Created on: Jun 10, 2012
+ *      Author: zeheron
+ */
+
+#ifndef UIDGENERATOR_H_
+#define UIDGENERATOR_H_
+
+class UIDGenerator {
+
+
+public:
+      static UIDGenerator * instance ();
+      int next () { return uid++; }
+
+private:
+      UIDGenerator () : uid(0) {}
+      virtual ~UIDGenerator();
+
+      static UIDGenerator * uidGen;
+      int uid;
+};
+
+#endif /* UIDGENERATOR_H_ */
